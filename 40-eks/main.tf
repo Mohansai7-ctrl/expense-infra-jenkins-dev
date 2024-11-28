4572628517c5f4e8f8604fd58699a1c26e1d5067
Mohansai <bandarumohansai@gmail.com>
2024-11-28 19:50:32 +0530

    added the eks public key value in 40-eks

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -12,8 +12,8 @@
 resource "aws_key_pair" "eks" {
   key_name   = "eks"
   # you can paste the public key directly like this
-  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/eks.pub")
+  #public_key = file("~/.ssh/eks.pub")
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM3pqvQPbcZR7JEYdlRXQ69FNULyCD73rpdShVdeh9VX HP@LAPTOP-I097EHJB"
   # ~ means windows home directory
 }
 
